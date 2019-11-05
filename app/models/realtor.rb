2ef1d2ead9844220918a8bb3be1ccea302128a4c
kakeru <okinawan.kkr0713@icloud.com>
2019-11-05 16:33:18 +0900

    Realtorのバリデーション記述

--- app/models/realtor.rb
+++ app/models/realtor.rb
@@ -1,2 +1,10 @@
 class Realtor < ApplicationRecord
+  
+  has_many :nearests_tations
+  validates :house, presence: true
+  validates :price,presence: true
+  validates :address,presence: true
+  validates :age,presence: true
+  validates :remarkes,presence: true
+
 end

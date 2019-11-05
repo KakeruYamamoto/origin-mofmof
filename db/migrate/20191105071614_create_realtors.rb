0d3c924da276c201d3c21c9bcda78ad5be7e8362
kakeru <okinawan.kkr0713@icloud.com>
2019-11-05 16:17:58 +0900

    migrateファイル記述、schemaファイル更新

--- db/migrate/20191105071614_create_realtors.rb
+++ db/migrate/20191105071614_create_realtors.rb
@@ -1,7 +1,12 @@
 class CreateRealtors < ActiveRecord::Migration[5.0]
   def change
     create_table :realtors do |t|
-
+      t.string :house
+      t.integer :price
+      t.text :address
+      t.integer :age
+      t.text :remarkes
+      
       t.timestamps
     end
   end

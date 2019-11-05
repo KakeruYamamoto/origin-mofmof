505e589e0c6594f2ad30e8baed52726564e78af3
kakeru <okinawan.kkr0713@icloud.com>
2019-11-05 16:38:07 +0900

    NearestStationのバリデーション記述

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,2 +1,8 @@
 class NearestStation < ApplicationRecord
+
+  belongs_to :realtor
+  validates :route_name, presence: true
+  validates :station_name, presence: true
+  validates :walking_minutes,  presence: true, length: { maximum: 3 }
+
 end

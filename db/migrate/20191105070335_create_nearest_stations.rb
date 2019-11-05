0bb6892cf7554dc6ffabb96cb8ed3b9a77f76189
kakeru <okinawan.kkr0713@icloud.com>
2019-11-05 16:07:18 +0900

    migrateファイル記述、schemaファイル更新

--- db/migrate/20191105070335_create_nearest_stations.rb
+++ db/migrate/20191105070335_create_nearest_stations.rb
@@ -1,6 +1,9 @@
 class CreateNearestStations < ActiveRecord::Migration[5.0]
   def change
     create_table :nearest_stations do |t|
+      t.string :route_name
+      t.string :station_name
+      t.integer :walking_minutes
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191105063727) do
+ActiveRecord::Schema.define(version: 20191105070335) do
+
+  create_table "nearest_stations", force: :cascade do |t|
+    t.string   "route_name"
+    t.string   "station_name"
+    t.integer  "walking_minutes"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string   "house"

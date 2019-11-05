e8d5437c7fa614b7c1f45b7bf13247abe72578b4
kakeru <okinawan.kkr0713@icloud.com>
2019-11-05 19:02:04 +0900

    model/realtorにaccepts_nested_attributes_for追記

--- app/models/realtor.rb
+++ app/models/realtor.rb
@@ -1,6 +1,7 @@
 class Realtor < ApplicationRecord
 
   has_many :nearests_tations
+  accepts_nested_attributes_for :nearests_tations
   validates :house, presence: true
   validates :price,presence: true
   validates :address,presence: true

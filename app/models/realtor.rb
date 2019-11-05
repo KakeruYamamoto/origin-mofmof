041a01632b3ce6aea3202d269e8a2bae33885eaf
kakeru <okinawan.kkr0713@icloud.com>
2019-11-05 16:38:53 +0900

    lengthバリデーション記述

--- app/models/realtor.rb
+++ app/models/realtor.rb
@@ -1,10 +1,10 @@
 class Realtor < ApplicationRecord
-  
+
   has_many :nearests_tations
   validates :house, presence: true
   validates :price,presence: true
   validates :address,presence: true
-  validates :age,presence: true
+  validates :age,presence: true, length: { maximum: 3 }
   validates :remarkes,presence: true
 
 end
